3ab6adf87a6d5af8693195d3883326bc7dc3ba91
dluga93 <dluga93@gmail.com>
2016-09-13 22:21:29 +0200

    Removed accidental uploads.

--- make.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "C:\\Users\\Dean\\Downloads\\Software\\Programming\\Pwd Manager"
-javac -cp ".;C:\\Users\\Dean\\Downloads\\Software\\Programming\\org.eclipse.swt32\\swt.jar;" "PwdManager\\Client.java"
