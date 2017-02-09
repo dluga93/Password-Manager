7c0025551697feb8999e39eba820c7c3e7861e52
dluga93 <dluga93@gmail.com>
2017-02-09 19:57:53 +0100

    Added ByteArray class as a wrapper around byte[].

---
+++ Password_Manager/ByteArray.java
@@ -0,0 +1,13 @@
+package Password_Manager;
+
+public class ByteArray {
+    private byte[] data;
+
+    public ByteArray(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+}
