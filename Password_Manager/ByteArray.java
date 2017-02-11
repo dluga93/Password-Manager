9b7792986f55e41cdff32ce4a23a6ee8d4b263c6
dluga93 <dluga93@gmail.com>
2017-02-11 16:25:40 +0100

    Added some documentation.

--- Password_Manager/ByteArray.java
+++ Password_Manager/ByteArray.java
@@ -1,24 +1,47 @@
 package Password_Manager;
 
+/**
+ * \brief Wrapper class for byte[]
+ */
 public class ByteArray {
     private byte[] data;
 
+    /**
+     * Constructs an object and initializes it with the ```data``` argument.
+     *
+     * @param      data  The byte array for initialization.
+     */
     public ByteArray(byte[] data) {
         this.data = data;
     }
 
+    /**
+     * Constructs an object as an empty array of size ```arraySize```
+     *
+     * @param      arraySize  The array size
+     */
     public ByteArray(int arraySize) {
         this.data = new byte[arraySize];
     }
 
+    /**
+     * @return     The raw byte array
+     */
     public byte[] getData() {
         return data;
     }
 
+    /**
+     * Sets the data in the byte array to the argument.
+     *
+     */
     public void setData(byte[] data) {
         this.data = data;
     }
 
+    /**
+     * @return     Number of elements in the array.
+     */
     public int length() {
         return data.length;
     }
--- Password_Manager/Client.java
+++ Password_Manager/Client.java
@@ -1,12 +1,18 @@
 /*
-Parts of the GUI code is based on a template given for an
+Part of the GUI code is based on a template given for an
 assignment in my Security course.
 */
+
+/**
+ * \mainpage Password Manager
+ * Simple password manager in Java
+ */
+
 package Password_Manager;
 import Password_Manager.UI.MainUI;
 
 /**
- * The main client.
+ * @brief The entry point of the program.
  */
 public class Client {
 	private static boolean restart = false;
@@ -18,6 +24,12 @@ public class Client {
 		} while (restart);
 	}//end main()
 
+	/**
+	 * @brief Sets the restart flag to true. 
+	 * 
+	 * @desc When the restart flag is true, the program will restart when the MainUI closes. 
+	 * This is used for going back to the login screen when an account is deleted.
+	 */
 	public static void setRestart() {
 		restart = true;
 	}
