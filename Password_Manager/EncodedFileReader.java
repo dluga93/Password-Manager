cec315981477767ee690263f34a6b4a3bcba0331
dluga93 <dluga93@gmail.com>
2017-02-11 17:31:41 +0100

    Finished documentation of EncodedFileReader.

--- Password_Manager/EncodedFileReader.java
+++ Password_Manager/EncodedFileReader.java
@@ -26,9 +26,10 @@ public class EncodedFileReader {
 	/**
 	 * @brief Closes the file we're reading.
 	 * 
-	 * Closes the fileInStream object.
+	 * Closes the fileInStream object. After the caller finished using the file,
+	 * this function should always be called to release system resources.
 	 *
-	 * @throws     IOException  if an error occured while closing the file stream.
+	 * @throws     IOException  if an error occurred while closing the file stream.
 	 */
 	public void close() throws IOException {
 		fileInStream.close();
@@ -41,7 +42,7 @@ public class EncodedFileReader {
 	 *
 	 * @return     A list of data entries as ```ByteArray``` objects.
 	 *
-	 * @throws     IOException  if a problem occured while reading from the file.
+	 * @throws     IOException  if a problem occurred while reading from the file.
 	 */
 	public ArrayList<ByteArray> readData() throws IOException {
 		ArrayList<ByteArray> dataEntries = new ArrayList<ByteArray>();
@@ -65,7 +66,7 @@ public class EncodedFileReader {
 	 *
 	 * @return     The data entry as a ```byte[]```
 	 *
-	 * @throws     IOException   If an error occured reading from the file
+	 * @throws     IOException   If an error occurred reading from the file
 	 * @throws     EOFException  If we've reached the end of the file.
 	 */
 	private byte[] decodeBytes() throws IOException, EOFException {
@@ -76,6 +77,18 @@ public class EncodedFileReader {
 		return data;
 	}
 
+	/**
+	 * @brief Returns a decoded integer from the file
+	 * 
+	 * Reads the next 4 bytes from the file, decodes them into
+	 * an integer and returns that integer. The way bytes are stored is
+	 * further explained in EncodedFileWriter.
+	 *
+	 * @return     the decoded integer
+	 *
+	 * @throws     IOException   If an error occurred reading from the file
+	 * @throws     EOFException  If we've reached the end of the file.
+	 */
 	private int decodeInt() throws IOException, EOFException {
 		byte[] intBytes = new byte[4];
 		if (fileInStream.read(intBytes) == -1)
@@ -83,6 +96,13 @@ public class EncodedFileReader {
 		return decodeInt(intBytes);
 	}
 
+	/**
+	 * @brief Returns an integer decoded from 4 bytes
+	 *
+	 * @param      encodedInt  The 4 byte array that will be decoded
+	 *
+	 * @return     decoded integer
+	 */
 	private int decodeInt(byte[] encodedInt) {
 		int decodedInt = 0;
 		for (int i = 0; i < 4; ++i) {
@@ -93,6 +113,20 @@ public class EncodedFileReader {
 		return decodedInt;
 	}
 
+	/**
+	 * @brief Return names of password files
+	 *
+	 * Returns the names of the files in the ```user```'s
+	 * password directory. This could cause problems if
+	 * a non-password file is contained in that directory.
+	 *
+	 * @param      user       The username
+	 *
+	 * @return     A list of filenames
+	 *
+	 * @throws     Exception  If the user's password directory
+	 * is not found.
+	 */
 	public static ArrayList<String> getFilenames(String user)
 	throws Exception {
 		File directory = new File(Naming.directoryName(user));
