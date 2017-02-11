de165d3cd4d34a565e208ad25d5a4d2405215260
dluga93 <dluga93@gmail.com>
2017-02-11 22:42:52 +0100

    Naming.java docs

--- Password_Manager/Naming.java
+++ Password_Manager/Naming.java
@@ -9,22 +9,58 @@ public class Naming {
 	private static final String directorySuffix = "_dir";
 	private static final String keyFileSuffix = "keys";
 
+	/**
+	 * @brief generate filename for master key salt
+	 *
+	 * @param      user  The username
+	 *
+	 * @return     Filename where salt will be stored
+	 */
 	public static String masterSaltFilename(String user) {
 		return user + masterSaltFileSuffix;
 	}
 
+	/**
+	 * @brief generate filename for mac key salt
+	 *
+	 * @param      user  The username
+	 *
+	 * @return     Filename where salt will be stored
+	 */
 	public static String macSaltFilename(String user) {
 		return user + macSaltFileSuffix;
 	}
 
+	/**
+	 * @brief generate name of password files directory
+	 *
+	 * @param      user  The username
+	 *
+	 * @return     Directory name
+	 */
 	public static String directoryName(String user) {
 		return user + directorySuffix;
 	}
 
+	/**
+	 * @brief generate name of password entry
+	 *
+	 * @param      user     The username
+	 * @param      website  The entry's website
+	 *
+	 * @return     entry filename
+	 */
 	public static String entryFilename(String user, String website) {
 		return user + "_" + website;
 	}
 
+	/**
+	 * @brief generate name of file to store keys
+	 *
+	 * @param      user  The username
+	 *
+	 * @return     Name of file to store keys
+	 */
 	public static String keyFileName(String user) {
 		return user + keyFileSuffix;
 	}
