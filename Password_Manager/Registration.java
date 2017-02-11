2ac815bd267e4afd504b7935bfe91700a49ad181
dluga93 <dluga93@gmail.com>
2017-02-11 22:09:57 +0100

    Added docs for Registration class. Throwing exception instead of logging error.

--- Password_Manager/Registration.java
+++ Password_Manager/Registration.java
@@ -6,14 +6,27 @@ import java.nio.file.*;
 import java.io.*;
 import java.util.*;
 
+/**
+ * @brief Class for registering a new user.
+ */
 public class Registration {
-	private static final int SALT_LENGTH = 32; // bytes
-	private byte[] encryptedMasterKey;
-	private byte[] encryptedMacKey;
-	private byte[] masterKey;
-	private byte[] macKey;
-	private String user;
+	private static final int SALT_LENGTH = 32; ///< Length of the salt in bytes
+	private byte[] encryptedMasterKey; ///< Encrypted master key, ready to be written to file
+	private byte[] encryptedMacKey; ///< Encrypted mac key, ready to be written to file
+	private byte[] masterKey; ///< generated master key (unencrypted)
+	private byte[] macKey; ///< generated mac key (unencrypted)
+	private String user; ///< username
 
+	/**
+	 * @brief Registers a new user
+	 * 
+	 * Register a new user with username ```usr``` and password
+	 * ```password```. This generates the master and mac keys, the salts 
+	 * for each, and creates the necessary files and directories.
+	 *
+	 * @param      usr       The username
+	 * @param      password  The user's password
+	 */
 	public Registration(String usr, String password)
 	throws FileAlreadyExistsException, Exception {
 		user = usr;
@@ -23,7 +36,18 @@ public class Registration {
 		encryptAndStoreKeys(password);
 	}
 
-	// register with a preset master and mac key
+	/**
+	 * @brief Registers a user with preset keys
+	 * 
+	 * Registers a user with preset master and mac keys. This function is called
+	 * when the user wants to change the password, so it uses the existing keys
+	 * and just re-encrypts them with a different password and salt.
+	 *
+	 * @param      usr        The username
+	 * @param      password   The new password
+	 * @param      masterKey  The master key
+	 * @param      macKey     The mac key
+	 */
 	public Registration(String usr, String password, byte[] masterKey, byte[] macKey)
 	throws FileAlreadyExistsException, Exception {
 		user = usr;
@@ -33,16 +57,41 @@ public class Registration {
 		encryptAndStoreKeys(password);
 	}
 
+	/**
+	 * @brief Check password validity
+	 * 
+	 * Checks the validity of ```password```. If the password is
+	 * shorter than 8 characters, it throws an exception.
+	 *
+	 * @param      password   The password to be checked.
+	 *
+	 * @throws     Exception  If the password is too short.
+	 */
 	private void assertValidPassword(String password) throws Exception {
 		if (password.length() < 8)
 			throw new Exception("Your password must be at least 8 characters long.");
 	}
 
+	/**
+	 * @brief Generate the keys
+	 * 
+	 * Generates the master key and the mac key that will
+	 * be used to encrypt the website password entries.
+	 */
 	private void createKeys() {
 		masterKey = CipherBuilder.generateKey(CipherBuilder.encryptionKeyType);
 		macKey = CipherBuilder.generateKey(Hmac.keyType);
 	}
 
+	/**
+	 * @brief Create password directory
+	 * 
+	 * Creates a directory based on the username to store the
+	 * password entries of the user.
+	 *
+	 * @throws     FileAlreadyExistsException  If a directory with the same name already exists.
+	 * @throws     Exception                   If an IO error occurred when creating the directory.
+	 */
 	private void createPasswordDirectory() throws FileAlreadyExistsException, Exception {
 		String directoryName = Naming.directoryName(user);
 		try {
@@ -54,6 +103,17 @@ public class Registration {
 		}
 	}
 
+	/**
+	 * @brief Encrypts and stores the keys
+	 * 
+	 * This function encrypts the master and mac keys with a master
+	 * password derived key and a randomly generated salt, and stores
+	 * them to a file generated based on the username.
+	 *
+	 * @param      password   The master password
+	 *
+	 * @throws     Exception  If an error occurred writing storing the keys.
+	 */
 	private void encryptAndStoreKeys(String password) throws Exception {
 		byte[] masterKeySalt = CipherBuilder.randomData(SALT_LENGTH);
 		byte[] macKeySalt = CipherBuilder.randomData(SALT_LENGTH);
@@ -70,6 +130,16 @@ public class Registration {
 		tryCreateFiles(masterKeySalt, macKeySalt);
 	}
 
+	/**
+	 * @brief Exception filter for createFiles
+	 * 
+	 * Calls createFiles and handles the exceptions that it might throw.
+	 *
+	 * @param      masterKeySalt               The master key salt
+	 * @param      macKeySalt                  The mac key salt
+	 *
+	 * @throws     FileAlreadyExistsException  If the salt or key files already exist.
+	 */
 	private void tryCreateFiles(byte[] masterKeySalt, byte[] macKeySalt)
 	throws FileAlreadyExistsException {
 		try {
@@ -77,11 +147,22 @@ public class Registration {
 		} catch (FileAlreadyExistsException e) {
 			throw e;
 		} catch (Exception e) {
-			Logger.logException("Can't create files for signing up.", e);
-			System.exit(1);
+			throw new Exception("Can't create files for signing up.", e);
 		}
 	}
 
+	/**
+	 * @brief Creates the salt and key files
+	 * 
+	 * Create files and store the two salts (unencrypted) and the
+	 * two keys (macced and encrypted).
+	 *
+	 * @param      masterKeySalt               The master key salt
+	 * @param      macKeySalt                  The mac key salt
+	 *
+	 * @throws     FileAlreadyExistsException  If one of the files already exists
+	 * @throws     Exception                   If an error occurs when writing to file
+	 */
 	private void createFiles(byte[] masterKeySalt, byte[] macKeySalt)
 	throws FileAlreadyExistsException, Exception {
 		saveDataToFile(masterKeySalt, Naming.masterSaltFilename(user));
@@ -95,6 +176,16 @@ public class Registration {
 		fileWriter.close();
 	}
 
+	/**
+	 * @brief Write a byte array to a file
+	 *
+	 * Writes the byte array ```data``` to the file ```filename```.
+	 *
+	 * @param      data       The data to write to file
+	 * @param      filename   The filename
+	 *
+	 * @throws     Exception  If an error occurred when writing to the file.
+	 */
 	private void saveDataToFile(byte[] data, String filename) throws Exception {
 		try {
 			EncodedFileWriter fileWriter = new EncodedFileWriter(filename);
