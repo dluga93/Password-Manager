17c4c6705929aec1155d8b338fc956e06391a30e
dluga93 <dluga93@gmail.com>
2017-02-12 00:12:43 +0100

    Docs for Encryption.Utility class.

--- Password_Manager/Encryption/Utility.java
+++ Password_Manager/Encryption/Utility.java
@@ -1,6 +1,15 @@
 package Password_Manager.Encryption;
 
+/**
+ * @brief Utility class for encryption and maccing
+ */
 public class Utility {
+    /**
+     * @brief Concatenate two byte arrys
+     *
+     * @return     An array containing the concatenated elements of the
+     * two arguments.
+     */
 	public static byte[] concatByteArray(byte[] array1, byte[] array2) {
 		byte[] concatenated = new byte[array1.length + array2.length];
 		System.arraycopy(array1, 0, concatenated, 0, array1.length);
